54a62ba55dddee3eae68e5d36d9d9b21504148bd
victor <victorandhadar@gmail.com>
2023-05-17 13:36:08 -0700

    finished project with tests

---
+++ TDDTests/StudentTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TDD.Tests
+{
+    [TestClass()]
+    public class StudentTests
+    {
+        [TestMethod()]
+        public void setAverageTest()
+        {
+            // Arrange
+            List<int> grades = new List<int> { 100, 100, 100, 100, 100 };
+            Student student = new Student("322149345", "victor", "socolovschi", "victor@gmail.com", "0547649355", grades);
+
+            float average = student.Average;
+
+            // Assert
+            Assert.AreEqual(average, 100);
+        }
+
+        [TestMethod()]
+        public void merge_test() {
+            List<float> grades1 = new List<float> { -100, -100, -70, -100, -100 };
+            List<float> grades2 = new List<float> { 34, -76, 90, 89, 100 };
+            List<float> grades3 = new List<float> { 100, 100, 70, 100, 100 };
+            List<Student> students = new List<Student>
+            {
+                new Student("322149233", "victor", "socolovschi", "victor@gmail.com", "0547649355", grades1),
+                new Student("322149345", "David", "magen", "david@gmail.com", "0547678987", grades2),
+                new Student("322149345", "yossi", "Aitan", "yossi@gmail.com", "0547634544", grades3),
+             };
+
+             List<float> mergedGrades = StudentForm.Merge(students);
+             List<float> expectedMergedGrades = new List<float> { -100, -100, -70, -100, -100, 34, -76, 90, 89, 100, 100, 70, 100, 100 };
+             CollectionAssert.AreEqual(expectedMergedGrades, mergedGrades);
+        }
+    }
+}
--- WindowsFormsApp1/Student.cs
+++ WindowsFormsApp1/Student.cs
@@ -8,6 +8,13 @@ namespace TDD
 {
     public class Student
     {
+        private string v1;
+        private string v2;
+        private string v3;
+        private string v4;
+        private string v5;
+        private List<int> grades;
+
         public string ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -34,6 +41,17 @@ namespace TDD
             }
             this.Average = (float)gradesSum/legalGrades;
         }
+
+        public Student(string v1, string v2, string v3, string v4, string v5, List<int> grades)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.grades = grades;
+        }
+
         public Boolean gradeIsLegal(int grade)
         {
             return grade != 777;
